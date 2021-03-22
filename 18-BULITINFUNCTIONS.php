d2560f35823cc1c2e4f5138c7e8356199fdeede9
Megha Garg <43913641+miss-jain-16@users.noreply.github.com>
2021-03-22 14:35:41 +0530

    Add files via upload

---
+++ 18-BULITINFUNCTIONS.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BUILT IN FUNCTIONS</title>
+</head>
+<body>
+<?php
+$var="MEGHA GARG";
+echo strrev($var);
+$c=100.8;
+$int_cast=(int)$c;
+echo $int_cast;
+?>
+</body>
+</html>
