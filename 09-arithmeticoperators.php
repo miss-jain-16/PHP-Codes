868f379ea83928643c8aba8400cae3ba76f8b8ae
Megha Garg <43913641+miss-jain-16@users.noreply.github.com>
2021-03-26 19:38:53 +0530

    Add files via upload

---
+++ 09-arithmeticoperators.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARITHMETIC</title>
+</head>
+<body>
+    <?php 
+    $a=9;
+    $b=10;
+    echo $a+$b;
+    echo "<br>";
+    echo $b-$a;
+    echo "<br>";
+    echo $b*$a;
+    echo "<br>";
+    echo $b/$a;
+    echo "<br>";
+    echo $a**$b;
+
+    ?>
+</body>
+</html>
